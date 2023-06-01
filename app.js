4f8696f6bb5f765c3dc9d4f1da38b2d426731c84
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-01 22:00:34 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ var fullModal = document.getElementById("exampleModalToggle")
 var textModal = document.getElementById("text-modal")
 var postText = document.getElementById("post-text")
 var inSidePostCard = document.getElementById("inSidePostCard")
+var bg_0 = document.getElementById("bg_0").className
+var bg_1 = document.getElementById("bg_1").className
+var bg_2 = document.getElementById("bg_2").className
+var bg_3 = document.getElementById("bg_3").className
 var bg_4 = document.getElementById("bg_4").className
 
-console.log(bg_4)
+var getBackGround = ""
+function clickBackground(backgroundColor) {
+    if (backgroundColor == "orange") {
+        getBackGround = "bg-0"
+        console.log('orange')
+    } else if (backgroundColor == "blue") {
+        getBackGround = "bg-1"
+        console.log('blue')
+    } else if (backgroundColor == "purple") {
+        getBackGround = "bg-2"
+        console.log('purple')
+    }
+    
 
+    console.log(getBackGround);
+}
 
 
 
@@ -14,14 +32,14 @@ function post() {
     inSidePostCard.innerHTML = `
             <div class="postCard">
             <img width="47px" height="50px" class="my-pic" src="pic/my-pic.jpg" alt="">
-            <p class="inline-block"><b>Ahmed Karim</b></p>
-    <div class="bg-3">
+            <p class="inline-block "><b>Ahmed Karim</b></p>
+    <div class="${getBackGround}">
     <h1>
     ${textModal.value}
     </h1>
     </div>
     </div>
-    `
+    ` + inSidePostCard.innerHTML
     textModal.value = ""
 }
 // var heading = document.createElement("h1")
