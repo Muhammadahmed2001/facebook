782b2f87c3ca6e843f564ebb4994842d913d2ea7
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-01 19:12:55 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,25 +2,16 @@ var fullModal = document.getElementById("exampleModalToggle")
 var textModal = document.getElementById("text-modal")
 var postText = document.getElementById("post-text")
 var inSidePostCard = document.getElementById("inSidePostCard")
-var bg = document.getElementById("bg-0").src
-console.log(bg)
+var bg_4 = document.getElementById("bg_4").className
+
+console.log(bg_4)
 
 
 
-// var bg_0 =  document.getElementById("bg-0")
-// var bgColor = ""
-// function bg_0() {
-    //     var bgColor = "bg-0"
-    //     return bgColor
-    // }
-    // function bg_1() {
-        //     var bgColor = "bg-1"
-        //     return bgColor
-        
-        // }
-        // console.log(bg_0)
-        function post() {
-            inSidePostCard.innerHTML = `
+
+
+function post() {
+    inSidePostCard.innerHTML = `
             <div class="postCard">
             <img width="47px" height="50px" class="my-pic" src="pic/my-pic.jpg" alt="">
             <p class="inline-block"><b>Ahmed Karim</b></p>
